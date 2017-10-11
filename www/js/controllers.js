31decc703867c11e71423b198fe3e28741a7c482
Paulo M Marinho <paulo.marinho@prof.infnet.edu.br>
2017-10-11 11:12:59 +0000

    aula

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,48 +1,37 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, Auth) {
-
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  // Form data for the login modal
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, Auth, $rootScope) {
+  
+  
+  var checaLogin = localStorage.getItem("user");
+  if (checaLogin && checaLogin.length > 0){
+    $rootScope.estaLogado = true;
+  }
   $scope.loginData = {};
-
-  // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope
   }).then(function(modal) {
     $scope.modal = modal;
   });
-
-  // Triggered in the login modal to close it
   $scope.closeLogin = function() {
     $scope.modal.hide();
   };
-
-  // Open the login modal
+$scope.logout = function(){
+  $rootScope.estaLogado = false;
+  localStorage.removeItem("user");
+}
   $scope.login = function() {
     $scope.modal.show();
   };
 
-  // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     Auth.verificaLogin($scope.loginData).then(ret => {
       console.log('voltei com :');
       console.log(ret);
+      $rootScope.estaLogado = true;
       Auth.salvaLocalStorage("user",JSON.stringify(ret.data));
-      Auth.salvaLocalStorage("token",ret.data.token);
-      
     });
-    
     console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
     $timeout(function() {
       $scope.closeLogin();
     }, 1000);
